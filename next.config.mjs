0347b1273fd93bf006bb928ab23a182969f0d9e9
Majidkhan3 <majidabid017@gmail.com>
2024-02-14 15:07:15 +0500

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
+const nextConfig = {
+    experimental:
+    {
 
-
-const nextConfig = {};
+        eslint: {
+            ignoreDuringBuilds: true,
+        },
+    }
+};
 
 export default nextConfig;
